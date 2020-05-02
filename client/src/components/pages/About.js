5d15b067aa503764af3ed1378126c62715f92867
barathrajd <barathdeva757@gmail.com>
2020-05-02 20:58:41 +0530

    [Barathraj]Ready to Deploy

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -3,11 +3,12 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>About this Applicaion</h1>
+      <h3 className='bg-dark p'>About this Applicaion</h3>
       <p className='my-1'>This is fullstack React app for keeping contacts</p>
-      <p className='bg-dark p'>
-        <strong>Version:</strong>1.0.0
+      <p>
+        <strong>Version:</strong> 1.0.0
       </p>
+      <br />
     </div>
   );
 };
